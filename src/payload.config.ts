611544be05922a9ee4860b4d8698291b74bf7ca3
mohamad44c <mchebli008@hotmail.com>
2026-02-26 00:09:51 +0200

    feat: add PostCSS configuration, global styles, and update package dependencies

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,12 +1,16 @@
-import { postgresAdapter } from '@payloadcms/db-postgres'
+import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
-import { Users } from './collections/Users'
+import { Categories } from './collections/Categories'
 import { Media } from './collections/Media'
+import { Users } from './collections/Users'
+import Products from './collections/Products'
+import Settings from './collections/Settings'
+import { Deals } from './collections/Deals'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -18,13 +22,13 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, Categories, Products, Deals, Settings],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: postgresAdapter({
+  db: vercelPostgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
     },
