8ad235fff66b5d5d10c825eb846c262e95225121
mohamad44c <mchebli008@hotmail.com>
2026-02-26 00:14:06 +0200

    feat: enhance layout structure with RootProvider and update HomePage to a simple Hello World component

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import './globals.css'
 
+import { Poppins, Amiri } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import { RootProvider } from '@/providers/root-provider'
+
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins',
+})
+
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <main>{children}</main>
+        <RootProvider>
+          <main>{children}</main>
+        </RootProvider>
       </body>
     </html>
   )
